9c4ab1a61f5962bc3639e0aeb2bace9c6f5c0a78
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-06-12 06:49:43 -0300

    Feat: Add Global navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { FaApple } from "react-icons/fa";
+import { IoBagOutline } from "react-icons/io5";
+import { BsSearch } from "react-icons/bs";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
+
 function App() {
-  return <h1 className="text-3xl font-bold">Hello World</h1>;
+  return (
+    <>
+      <div>
+        <nav className="bg-[#161617cc] text-[#ffffffcc] h-12 ">
+          <div className="flex h-full">
+            <ul className="flex flex-1">
+              <li className="flex-1">
+                <a href="#" className="block w-max px-4 h-12">
+                  <FaApple className="h-full" />
+                </a>
+              </li>
+              <li>
+                <a href="#" className="block w-max px-4 h-12">
+                  <BsSearch className="h-full" />
+                </a>
+              </li>
+              <li>
+                <a href="#" className="block w-max px-4 h-12">
+                  <IoBagOutline className="h-full" />
+                </a>
+              </li>
+            </ul>
+            <div>
+              <button className="block w-max px-4 h-12" aria-label="Menu">
+                <HiOutlineMenuAlt4 className="h-full" />
+              </button>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
 }
 
 export default App;
