b77f3168ee6d76a04d3c8c4d0dbbaad12be2bc00
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-06-13 13:50:15 -0300

    Refactor: Move components to GlobalNav

--- src/App.tsx
+++ src/App.tsx
@@ -2,113 +2,8 @@ import { FaApple } from "react-icons/fa";
 import { IoBagOutline } from "react-icons/io5";
 import { BsSearch } from "react-icons/bs";
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
-import React, { useEffect, useState } from "react";
-
-const MenuItem = ({
-  children,
-  show,
-  index,
-}: {
-  children: ((showSubMenu: boolean) => React.ReactNode) | React.ReactNode;
-  show: boolean;
-  index: number;
-}) => {
-  const [showSubMenu, setShowSubMenu] = useState(false);
-
-  useEffect(() => {
-    if (!show) {
-      setShowSubMenu(false);
-    }
-  }, [show]);
-
-  return (
-    <div
-      className={`text-2xl px-12 py-1 font-semibold duration-1000 ${
-        show ? "opacity-100" : "opacity-0"
-      }`}
-      style={{
-        transitionDelay: show ? `${index * 50}ms` : "0ms",
-      }}
-      role="button"
-      onClick={() => !showSubMenu && setShowSubMenu(!showSubMenu)}
-    >
-      {typeof children === "function"
-        ? children(show && showSubMenu)
-        : children}
-    </div>
-  );
-};
-
-const SubMenuItem = ({
-  active,
-  subMenu,
-}: {
-  active: boolean;
-  subMenu: string[];
-}) => {
-  return (
-    <div
-      className={`pt-12 absolute z-20 bg-[#161617] w-full h-full top-0 left-0 right-0 ${
-        active ? "visible" : "invisible"
-      }`}
-    >
-      <ul>
-        {subMenu.map((item, index) => (
-          <li key={index} className="text-2xl font-semibold px-12 py-1">
-            <a href="#">{item}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-const GlobalNavContent = ({ show }: { show: boolean }) => {
-  const items = [
-    {
-      label: "Store",
-      subMenu: [
-        "Shop the Latest",
-        "Mac",
-        "iPad",
-        "iPhone",
-        "Apple Watch",
-        "Accessories",
-      ],
-    },
-    { label: "Mac", subMenu: [] },
-    { label: "iPad", subMenu: [] },
-    { label: "iPhone", subMenu: [] },
-    { label: "Watch", subMenu: [] },
-    { label: "Vision", subMenu: [] },
-    { label: "AirPods", subMenu: [] },
-    { label: "TV & Home", subMenu: [] },
-    { label: "Entertaiment", subMenu: [] },
-    { label: "Accessories", subMenu: [] },
-    { label: "Support", subMenu: [] },
-  ];
-
-  return (
-    <li
-      className={`absolute bg-[#161617] z-10 w-full transition-all duration-[600ms] ${
-        show ? "h-screen visible" : "h-0 invisible"
-      }`}
-    >
-      <div className="pt-12 relative">
-        {items.map((item, index) => (
-          <MenuItem show={show} index={index} key={index}>
-            {(active) => (
-              <div>
-                <span>{item.label}</span>
-                <SubMenuItem active={active} subMenu={item.subMenu} />
-              </div>
-            )}
-          </MenuItem>
-        ))}
-      </div>
-    </li>
-  );
-};
+import { useState } from "react";
+import { GlobalNav } from "./GlobalNav";
 
 function App() {
   const [show, setShow] = useState(false);
@@ -135,7 +30,7 @@ function App() {
                   <FaApple className="h-full" />
                 </a>
               </li>
-              <GlobalNavContent show={show} />
+              <GlobalNav show={show} />
               <li>
                 <a href="#" className="block w-max px-4 h-12">
                   <BsSearch className="h-full" />
